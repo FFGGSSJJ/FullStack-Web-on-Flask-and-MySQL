27ff3334f2bdd2dad5a14ee1dbcbce3a3fb134ce
FF_GG <Guanshujie.19@intl.zju.edu.cn>
2022-08-01 15:45:59 -0700

    update template code

--- Project/app/routes.py
+++ Project/app/routes.py
@@ -71,6 +71,17 @@ def advanced_1():
     result = {'success': True, 'response': 'Done'}
     return jsonify(result)
 
+@app.route("/logincheck", methods=['POST'])
+def logincheck():
+    """ logincheck """
+    data = request.get_json()
+    userinfo = db_helper.search_user(data)
+    if userinfo == {}:
+        return jsonify({'success': False,'response': 'Incorrect username or password'})
+    else:
+        return jsonify({'success': True,'response': 'User info was found.'})
+
+
 # Page routes
 @app.route("/search_page")
 def search_page():
@@ -141,9 +152,14 @@ def check_user():
     try:
         data = request.get_json()
         user = db_helper.search_user(data)
+        if user == {}:
+            print("User not found")
+            result = {'success': False, 'response': 'User not found'}
+        else:
+            print("User found")
+            result = {'success': True, 'response': 'Done'}
         session.clear()
         session['adv_query_1'] = user
-        result = {'success': True, 'response': 'Done'}
     except:
         result = {'success': False, 'response': 'Something went wrong'}
     return jsonify(result)
--- Project/app/static/script/logreg.js
+++ Project/app/static/script/logreg.js
@@ -2,16 +2,23 @@ $(document).ready(function () {
     // login button function
     $('#loginbtn').click(function () {
         console.log('login clicked');
+        console.log($('#userid').val());
         $.ajax({
             type: "POST",
-            url: "/login",
+            url: "/check_user",
             contentType: "application/json; charset=utf-8",
             data: JSON.stringify({
-
+                'userID': $('#userid').val(),
+                'account_passwd': $('#password').val()
             }),
             success: function (res) {
                 console.log(res.response)
-                location.href = '/homepage';
+                if (res.success) {
+                    location.href = '/';
+                } else {
+                    location.reload();
+                    console.log('User not found');
+                }
             },
             error: function () {
                 console.log('Error');
